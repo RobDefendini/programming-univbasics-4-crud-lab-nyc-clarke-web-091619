d15d6a8fd6592c178a914d7164a1f499ac3ab0a0
persuasive-vector-4859 <robdefendini@gmail.com>
2020-03-01 23:06:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,16 +1,20 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  [ "alpha" , "beta" , "capa" , "delta" ]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+array = ["wow", "I", "am", "really", "learning"]
+    element = "arrays!"
+array << element 
 end
 
 def add_element_to_start_of_array(array, element)
+  array = ["I", "am", "really", "learning"]
+    element = "wow"
   
 end
 
